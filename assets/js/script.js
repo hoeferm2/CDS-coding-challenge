46974b8ffde93a1bd5e86379c871f940cde64a80
Hoefer <ningboninja@gmail.com>
2022-11-16 21:38:55 -0800

    form works, input from dropdown menu not logging, will need to script an if else contigency for when either promo code filled or how you heard about us question is answered

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const formElement = $('#info-form')
+const formEl = $('#info-form')
 const firstNameEl = $('input[name="first-name"]')
 const lastNameEl = $('input[name="last-name"]')
 const phoneNumEl = $('input[name="phone-number"]')
@@ -6,3 +6,16 @@ const emailEl = $('input[name="email-address"]')
 const promoCodeEl = $('input[name="promo-code"]')
 const howDidEl = $('input[name="how-did"]')
 const termsConditionsEl = $('input[name="terms-conditions"]')
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    console.log('First Name:', firstNameEl.val());
+    console.log('Last Name:', lastNameEl.val());
+    console.log('Phone Number:', phoneNumEl.val());
+    console.log('Email:', emailEl.val());
+    console.log('Promo Code:', promoCodeEl.val());
+    console.log('How you found out about us:',howDidEl.val())
+}
+
+formEl.on('submit', handleFormSubmit)
